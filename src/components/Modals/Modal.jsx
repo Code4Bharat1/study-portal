baffaf56a23779b1cc94680e7388b4a88f01bc99
Faraaz Kurawle <kurawlefaraaz@gmail.com>
2025-06-03 22:10:13 +0530

    Increased Width of instrcutions for better UI

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -10,11 +10,11 @@ export default function Modal({ children, onClose, ariaLabel }) {
         role="dialog"
         aria-modal="true"
         aria-label={ariaLabel}
-        className="fixed my-5 top-1/2 left-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-xl p-6 max-w-lg w-[90%]"
+        className="fixed my-5 top-1/2 left-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-xl p-6  w-[50%]"
       >
         <button
           onClick={onClose}
-          className="absolute top-3 right-3 text-red-600 hover:text-red-800 text-3xl font-bold leading-none"
+          className="absolute top-0  right-1 text-red-600 hover:text-red-800 text-4xl font-bold leading-none"
           aria-label="Close modal"
         >
           &times;
